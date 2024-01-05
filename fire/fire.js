e0292e266a2c80c75f20e200fba35fbc90b70a4b
AnastasiiaShkala <nastyashkala@gmail.com>
2024-01-04 16:53:44 -0800

    first commit

---
+++ fire/fire.js
@@ -0,0 +1,28 @@
+const button = document.querySelector('#myButton');
+button.addEventListener('click', function() {
+    document.querySelector('#player').play();
+    document.querySelector('#myVideo').play();
+})
+
+const timer = 3;
+let amountTime = timer * 60;
+
+function calculateTime(){
+    const countdownFire = document.querySelector('#countdown-fire');
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    } 
+    countdownFire.textContent = `${minutes} : ${seconds}`;
+    amountTime--;
+    if(amountTime < 0){
+        stopTimer();
+        amountTime = 0;
+    }
+    function stopTimer(){
+        clearInterval(timerID);
+    }
+}
+let timerID = setInterval(calculateTime, 1000);
